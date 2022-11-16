e094b643f24d428bc8bd1b6cf85bf45e1d389984
Arnold Southammavong <arnoldsouth@gmail.com>
2022-11-16 06:33:51 -0800

    Update Socials component

--- components/socials.js
+++ components/socials.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 
 import { SiGithub, SiLinkedin, SiTwitter } from "react-icons/si";
+import { CgNotes } from "react-icons/cg";
+import { GrMail } from "react-icons/gr";
 
 import layoutStyles from "./layout.module.css";
 
@@ -39,6 +41,28 @@ export default function Socials() {
           href="https://twitter.com/arnold_rip/"
         >
           <SiTwitter />
+        </a>
+      </div>
+
+      <div>
+        <a
+          // className={`${layoutStyles.iconSocials} ${layoutStyles.grid}`}
+          className={layoutStyles.iconSocials}
+          target="_blank"
+          href="https://docs.google.com/document/d/1UXpZQqikA8QX5kw-DS8WyAGVdN3wi6ts/edit?usp=sharing&ouid=104290255129280048353&rtpof=true&sd=true"
+        >
+          <CgNotes />
+        </a>
+      </div>
+
+      <div>
+        <a
+          // className={`${layoutStyles.iconSocials} ${layoutStyles.grid}`}
+          className={layoutStyles.iconSocials}
+          target="_blank"
+          href="mailto:arnoldsouth@gmail.com"
+        >
+          <GrMail />
         </a>
       </div>
     </div>
